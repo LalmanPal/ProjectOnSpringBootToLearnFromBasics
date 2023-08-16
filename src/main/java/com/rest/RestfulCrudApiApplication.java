9b60fbea444922c2ac30f136615c099ecd0194f0
Lalman <lalmanpal004@gmail.com>
2023-08-16 14:00:27 +0530

    My commit on 16-08-2023 by lalaman' ;

---
+++ src/main/java/com/rest/RestfulCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulCrudApiApplication.class, args);
+	}
+
+}
